1f83eff4d11640a0d85112eba57698d4533dbd1f
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-05-26 19:59:37 -0300

    Change amount btn to navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import { Nav } from "./components/Nav/Nav";
 import { Product } from "./components/Product/Product";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Nav />
       <Product />
     </div>
   );
--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,11 +2,11 @@ import React from "react";
 import { useEffect, useState } from "react";
 import data from "../../data/data.json";
 import "./style.css";
+import { Nav } from "../Nav/Nav";
 
 import { IoMdHeartEmpty } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
 import arrowr from "./right-arrow.svg";
-import circle from "./red-circle.svg";
 
 export function Product() {
   const [response, setResponse] = useState([]);
@@ -46,98 +46,100 @@ export function Product() {
     setInstallment(instRemove);
   };
   return (
-    <div className="container">
-      <section className="cart">
-        <span className="amount">
-          <img src={circle} alt="circle" />
-          <span className="amount-text">{amount}</span>
-        </span>
+    <>
+      <Nav amount={amount} />
+
+      <div className="container">
+        <section className="cart">
+          <ul>
+            {cart.map((item, index) => (
+              <li className="cart-product" key={index}>
+                <button
+                  value={index}
+                  onClick={() => handleRemoveItem(item, index)}
+                >
+                  X
+                </button>
+                <img src={item.product.images[0]} alt="img" />
+                <section className="cart-text">
+                  <p className="cart-name">{item.product.name}</p>
+                  <p>
+                    {item.product.price.installments}x R${" "}
+                    {item.product.price.installmentValue}
+                  </p>
+                  <p>ou R$ {item.product.price.value} à vista</p>
+                </section>
+              </li>
+            ))}
+          </ul>
+          <div className="subtotal">
+            <p>subtotal</p>
+          </div>
+          <p className="subtotal-value">10x R${installment}</p>
+          <p className="subtotal-value">ou R$ {subtotal} à vista</p>
+        </section>
+
         <ul>
-          {cart.map((item, index) => (
-            <li className="cart-product" key={index}>
-              <button
-                value={index}
-                onClick={() => handleRemoveItem(item, index)}
-              >
-                X
-              </button>
-              <img src={item.product.images[0]} alt="img" />
-              <section className="cart-text">
-                <p className="cart-name">{item.product.name}</p>
-                <p>
-                  {item.product.price.installments}x R${" "}
-                  {item.product.price.installmentValue}
+          {response.map((item, index) => (
+            <li key={index} className="cards">
+              <section className="product-img">
+                <section className="product-images">
+                  {item.product.images.map((_, index) => (
+                    <img
+                      id="s-img"
+                      key={index}
+                      src={item.product.images[index]}
+                      alt="img"
+                    />
+                  ))}
+                </section>
+                <img src={item.product.images[0]} alt="img" />
+              </section>
+
+              <section className="card-info">
+                <p className="product-name">
+                  {item.product.name} <IoMdHeartEmpty className="heart" />
                 </p>
-                <p>ou R$ {item.product.price.value} à vista</p>
+
+                <section className="product-prices">
+                  <section>
+                    <div className="arrow-tag">
+                      <p>MELHOR PREÇO</p>
+                      <img src={arrowr} alt="arrow" />
+                    </div>
+
+                    <span className="installments">
+                      {item.product.price.installments}x R$
+                      <span className="installment-value">
+                        {item.product.price.installmentValue}
+                      </span>
+                    </span>
+
+                    <span className="price-value">
+                      ou{" "}
+                      <span className="price">
+                        R$ {item.product.price.value}
+                      </span>{" "}
+                      à vista
+                    </span>
+                  </section>
+
+                  <section className="card-product-button">
+                    <button
+                      value={item}
+                      type="submit"
+                      onClick={() => handleAdd(item)}
+                    >
+                      Adicionar ao carrinho
+                      <IoIosArrowForward className="arrow" />
+                    </button>
+                  </section>
+                </section>
               </section>
             </li>
           ))}
         </ul>
-        <div className="subtotal">
-          <p>subtotal</p>
-        </div>
-        <p className="subtotal-value">10x R${installment}</p>
-        <p className="subtotal-value">ou R$ {subtotal} à vista</p>
-      </section>
-
-      <ul>
-        {response.map((item, index) => (
-          <li key={index} className="cards">
-            <section className="product-img">
-              <section className="product-images">
-                {item.product.images.map((_, index) => (
-                  <img
-                    id="s-img"
-                    key={index}
-                    src={item.product.images[index]}
-                    alt="img"
-                  />
-                ))}
-              </section>
-              <img src={item.product.images[0]} alt="img" />
-            </section>
-
-            <section className="card-info">
-              <p className="product-name">
-                {item.product.name} <IoMdHeartEmpty className="heart" />
-              </p>
-
-              <section className="product-prices">
-                <section>
-                  <div className="arrow-tag">
-                    <p>MELHOR PREÇO</p>
-                    <img src={arrowr} alt="arrow" />
-                  </div>
-
-                  <span className="installments">
-                    {item.product.price.installments}x R$
-                    <span className="installment-value">
-                      {item.product.price.installmentValue}
-                    </span>
-                  </span>
-
-                  <span className="price-value">
-                    ou{" "}
-                    <span className="price">R$ {item.product.price.value}</span>{" "}
-                    à vista
-                  </span>
-                </section>
-
-                <section className="card-product-button">
-                  <button
-                    value={item}
-                    type="submit"
-                    onClick={() => handleAdd(item)}
-                  >
-                    Adicionar ao carrinho
-                    <IoIosArrowForward className="arrow" />
-                  </button>
-                </section>
-              </section>
-            </section>
-          </li>
-        ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }
