4d93194f867ec2ccf8a412c827dfcfe667659f81
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-05-26 18:04:19 -0300

    Create amount folder

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,11 +1,18 @@
 import React from "react";
 import logo from "./logo-buscape.png";
+import menu from "./menu-button.svg";
 import "./style.css";
 
-export function Nav() {
+export function Nav({ amount }) {
   return (
     <nav>
       <img src={logo} alt="logo" className="logo" />
+      <img className="menu-btn" src={menu} alt="menu" />
+
+      {/* <span className="amount">
+        <img src={circle} alt="circle" />
+        <span className="amount-text">{amount}</span>
+      </span> */}
     </nav>
   );
 }
