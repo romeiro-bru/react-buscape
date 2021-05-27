60b150c08b835cd94cc9f0a438dc1257112a1379
Bruna Romeiro <56081906+romeiro-bru@users.noreply.github.com>
2021-05-27 15:42:17 -0300

    Add showCart state

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,19 +1,13 @@
 import React from "react";
 import logo from "./logo-buscape.png";
 import menu from "./menu-button.svg";
-import circle from "./red-circle.svg";
 import "./style.css";
 
-export function Nav({ amount }) {
+export function Nav() {
   return (
     <nav>
       <img src={logo} alt="logo" className="logo" />
       <img className="menu-btn" src={menu} alt="menu" />
-
-      <span hidden={amount === 0} className="amount">
-        <img src={circle} alt="circle" />
-        <button className="amount-text">{amount}</button>
-      </span>
     </nav>
   );
 }
